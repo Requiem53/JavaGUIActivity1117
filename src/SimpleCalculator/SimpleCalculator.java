3da48b10c87a4e098e420e3fd9ec448f48e76e71
LAB12_USER ON WS16 <L12X14W16@citulab.net>
2023-11-17 14:26:34 +0800

    A

--- src/SimpleCalculator/SimpleCalculator.java
+++ src/SimpleCalculator/SimpleCalculator.java
@@ -1,4 +1,35 @@
 package SimpleCalculator;
 
-public class SimpleCalculator {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleCalculator extends JFrame{
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+    private JPanel panel1;
+    private JButton btnCompute;
+
+    public SimpleCalculator() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float num1 = Float.parseFloat(tfNumber1.getText());
+                float num2 = Float.parseFloat(tfNumber2.getText());
+                char op = 
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SimpleCalculator app = new SimpleCalculator();
+        app.setContentPane(app.panel1);
+        JButton btn = new JButton("HIEFS");
+        app.setSize(800, 200);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Hello World");
+        app.setVisible(true);
+    }
 }
