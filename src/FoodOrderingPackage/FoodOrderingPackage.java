8d4d324ed533418a3a4b712c467a51cc925b988b
LAB12_USER ON WS16 <L12X14W16@citulab.net>
2023-11-17 14:58:57 +0800

    Button button

--- src/FoodOrderingPackage/FoodOrderingPackage.java
+++ src/FoodOrderingPackage/FoodOrderingPackage.java
@@ -6,4 +6,11 @@ public class FoodOrderingPackage {
     private JCheckBox cPizza;
     private JCheckBox cBurger;
     private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rbNone;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
 }
