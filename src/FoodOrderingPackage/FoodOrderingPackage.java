ee39d2979d486554c949dcf3c80861451c857fed
LAB12_USER ON WS16 <L12X14W16@citulab.net>
2023-11-17 14:53:59 +0800

    Stuff

---
+++ src/FoodOrderingPackage/FoodOrderingPackage.java
@@ -0,0 +1,9 @@
+package FoodOrderingPackage;
+
+import javax.swing.*;
+
+public class FoodOrderingPackage {
+    private JCheckBox cPizza;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+}
--- src/SimpleCalculator/SimpleCalculator.java
+++ src/SimpleCalculator/SimpleCalculator.java
@@ -3,6 +3,7 @@ package SimpleCalculator;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.Math;
 
 public class SimpleCalculator extends JFrame{
     private JTextField tfNumber1;
@@ -13,12 +14,31 @@ public class SimpleCalculator extends JFrame{
     private JButton btnCompute;
 
     public SimpleCalculator() {
+        lblResult.setEditable(false);
         btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                float num1 = Float.parseFloat(tfNumber1.getText());
-                float num2 = Float.parseFloat(tfNumber2.getText());
-                char op = 
+                try{
+                    float num1 = Float.parseFloat(tfNumber1.getText());
+                    float num2 = Float.parseFloat(tfNumber2.getText());
+                    int op = cbOperations.getSelectedIndex();
+                    float res;
+                    if(op == 0){
+                        res = num1 + num2;
+                    }else if(op == 1){
+                        res = num1 - num2;
+                    }else if(op == 2){
+                        res = num1 * num2;
+                    }else{
+                        if(num2 == 0){
+                            throw new IllegalArgumentException("Denominator be non-zero");
+                        }
+                        res = num1 / num2;
+                    }
+                    lblResult.setText(String.format("%.2f", res));
+                }catch (Exception ex){
+                    JOptionPane.showMessageDialog(panel1, ex.getMessage());
+                }
             }
         });
     }
